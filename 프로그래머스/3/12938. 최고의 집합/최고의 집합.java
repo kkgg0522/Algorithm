fdd3ad4ec1c75876aa8e5a28ce84ad6af9e736f7
Wonhyeong Kim <81723555+kkgg0522@users.noreply.github.com>
2025-03-17 20:24:32 +0900

    [level 3] Title: 최고의 집합, Time: 0.25 ms, Memory: 54.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int s) {
+        if (n > s) return new int[]{-1};
+
+        int[] answer = new int[n];
+
+        int base = s / n;
+        int remainder = s % n;
+
+        Arrays.fill(answer, base);
+
+        for (int i = 0; i < remainder; i++) {
+            answer[n - 1 - i]++;
+        }
+
+        return answer;
+    }
+}
